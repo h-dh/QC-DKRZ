4fdb7aaab2c69d1a67a080d49d8fd31a470667b2
h-dh <hollweg@dkrz.de>
2016-02-11 09:49:32 +0100

    Annotation: variable selection back to basics

--- src/InFile.cpp
+++ src/InFile.cpp
@@ -581,7 +581,7 @@ InFile::init(void)
       std::vector<std::string> value;
       std::vector<size_t> xRecord_01;
 
-     	notes->push_front(key , "*", freq, level, true, task, "", value,
+     	notes->push_front(key , "*", freq, level, task, "", value,
 	                         xRecord_01, xRecord_01);
       notes->inq(key);
 
