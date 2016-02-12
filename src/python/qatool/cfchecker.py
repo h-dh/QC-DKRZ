1cffd2b1eb99958de6ea925b95ee0f6eb21468d6
Carsten Ehbrecht <ehbrecht@dkrz.de>
2016-02-12 17:09:56 +0100

    added initial command line template

--- src/python/qatool/cfchecker.py
+++ src/python/qatool/cfchecker.py
@@ -1,2 +1,44 @@
+import argparse
+import argcomplete
+
+import logging
+logging.basicConfig(format='%(message)s', level=logging.WARN)
+logger = logging.getLogger(__name__)
+
+
+def create_parser():
+    parser = argparse.ArgumentParser(
+        prog="cfchecker",
+        usage='''cfchecker [<options>] [<args>]''',
+        description="Checks NetCDF files to the Climate and Forecast Conventions.",
+        )
+    parser.add_argument("-v",
+                        dest="verbose",
+                        help="enable verbose mode",
+                        action="store_true")
+    parser.add_argument("--service",
+                        dest='service',
+                        required=False,
+                        type=type(''),
+                        default='http://localhost:8983/solr/birdhouse',
+                        help="Solr URL. Default: http://localhost:8983/solr/birdhouse",
+                        action="store")
+
+    return parser
+
+
+def execute(args):
+    if args.verbose:
+        logger.setLevel(logging.DEBUG)
+
+    logger.info('Done.')
+
+        
 def main():
-  pass
+    logger.setLevel(logging.INFO)
+
+    parser = create_parser()
+    argcomplete.autocomplete(parser)
+
+    args = parser.parse_args()
+    execute(args)
