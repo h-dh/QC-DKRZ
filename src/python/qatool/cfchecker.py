b011ba38ecad6782f87a29af6c05378d6001a63d
Carsten Ehbrecht <ehbrecht@dkrz.de>
2016-02-12 19:10:54 +0100

    added call to dkrz-cf-checker

--- src/python/qatool/cfchecker.py
+++ src/python/qatool/cfchecker.py
@@ -1,5 +1,8 @@
 import argparse
 import argcomplete
+import subprocess
+
+from qatool import __version__
 
 import logging
 logging.basicConfig(format='%(message)s', level=logging.WARN)
@@ -9,29 +12,37 @@ logger = logging.getLogger(__name__)
 def create_parser():
     parser = argparse.ArgumentParser(
         prog="cfchecker",
-        usage='''cfchecker [<options>] [<args>]''',
-        description="Checks NetCDF files to the Climate and Forecast Conventions.",
+        description="Checks compliance of climate data (NetCDF files) for Climate and Forecast conventions (http://cfconventions.org).",
         )
-    parser.add_argument("-v",
-                        dest="verbose",
-                        help="enable verbose mode",
-                        action="store_true")
-    parser.add_argument("--service",
-                        dest='service',
-                        required=False,
-                        type=type(''),
-                        default='http://localhost:8983/solr/birdhouse',
-                        help="Solr URL. Default: http://localhost:8983/solr/birdhouse",
-                        action="store")
+    parser.add_argument('-V', '--version', action='store_true',
+                        help='Display the cfhecker version information.')
+    parser.add_argument('--verbose' , '-v', action="count",
+                        help="Increase output. May be specified up to three times.")
+    parser.add_argument('-R', dest='extra_checks', action='store_true',
+                        help='Apply also recommendations given by CF conventions.')
+    parser.add_argument('-F', dest='path', action='store',
+                        help='Finds recursively all NetCDF files from starting point PATH.')
+    parser.add_argument('ncfile', nargs='*', help= "Defines the location of the dataset to be checked.")
 
     return parser
 
 
 def execute(args):
-    if args.verbose:
+    if args.version:
+        print("DKRZ cfchecker version %s" % __version__)
+        return 0
+    if args.verbose == 1:
+        logger.setLevel(logging.INFO)
+    elif args.verbose == 2:
         logger.setLevel(logging.DEBUG)
-
-    logger.info('Done.')
+    cmd = ['dkrz-cf-checker']
+    if args.ncfile:
+        cmd.extend(args.ncfile)
+    try:
+        subprocess.check_output(cmd)
+    except subprocess.CalledProcessError as err:
+        print err.output
+    return 0
 
         
 def main():
