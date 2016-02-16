f692d751f5f81cfb7e6f169d76d6f02809c4e814
Carsten Ehbrecht <ehbrecht@dkrz.de>
2016-02-16 15:15:19 +0100

    added output parameter

--- src/python/qatool/cfchecker.py
+++ src/python/qatool/cfchecker.py
@@ -22,9 +22,21 @@ def create_parser():
                         help='Apply also recommendations given by CF conventions.')
     parser.add_argument('-F', dest='path', action='store',
                         help='Finds recursively all NetCDF files from starting point PATH.')
+    parser.add_argument('-f', '--format', default='text', choices=['text', 'html', 'json'], action='store',
+                        help="Output format.")
+    parser.add_argument('-o', '--output', default='-', action='store',
+                        help="Output filename. Default = '-' (standard output)")
     parser.add_argument('ncfile', nargs='*', help= "Defines the location of the dataset to be checked.")
 
     return parser
+
+
+def write_output(filename='-', output=None):
+    if filename == '-':
+        print output
+    else:
+        with open(filename, 'w') as fp:
+            fp.write(output)
 
 
 def execute(args):
@@ -39,9 +51,10 @@ def execute(args):
     if args.ncfile:
         cmd.extend(args.ncfile)
     try:
-        subprocess.check_output(cmd)
+        output = subprocess.check_output(cmd)
+        write_output(filename=args.output, output=output)
     except subprocess.CalledProcessError as err:
-        print err.output
+        write_output(filename=args.output, output=err.output)
     return 0
 
         
