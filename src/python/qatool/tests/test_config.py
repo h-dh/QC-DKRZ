d5aac1eb14dee41c80a0b0408b8b3ce85be2683d
Carsten Ehbrecht <ehbrecht@dkrz.de>
2016-02-16 17:45:12 +0100

    added more config options

--- src/python/qatool/tests/test_config.py
+++ src/python/qatool/tests/test_config.py
@@ -7,6 +7,8 @@ def test_load_config():
     config.write_options()
     assert config.get_config_value('settings', 'project_data') == '/tmp/data'
     assert config.get_config_value('settings', 'use_strict') == False
+    assert config.get_config_value('settings', 'use_strict2') == None
     assert config.get_config_value('checks', 'cf') == '1.4'
     assert config.get_config_value('processing', 'nice') == '15'
+    #assert False
     
