b8cdd8023f4e3c70b63a5921e0886f7deaf3ef96
Carsten Ehbrecht <ehbrecht@dkrz.de>
2016-02-17 17:18:28 +0100

    cleaned up cf and base

--- src/python/qatool/cf.py
+++ src/python/qatool/cf.py
@@ -7,28 +7,13 @@ logger = logging.getLogger(__name__)
 
 class CFCheck(BaseCheck):
 
-    def __init__(self, verbose=False, criteria='normal', output_filename='-', output_format='text'):
-        self.verbose = verbose
-        self.criteria = criteria
-        self.output_filename = output_filename
-        self.output_format = output_format
-    
-
-    def write_output(self, output=None):
-        if self.output_filename == '-':
-            print output
-        else:
-            with open(self.output_filename, 'w') as fp:
-                fp.write(output)
-
-
-    def run(self, ds_loc):
+    def run(self, ds):
         cmd = ['dkrz-cf-checker']
         if self.verbose:
             cmd.append('--debug')
         if self.criteria == 'strict':
             cmd.append('-R')
-        cmd.append(ds_loc)
+        cmd.append(ds)
        
         try:
             logger.info('running %s', cmd)
