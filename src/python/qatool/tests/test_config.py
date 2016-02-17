6591dc4b3cede2955c28c0c4c9e1e1a96ba47722
Carsten Ehbrecht <ehbrecht@dkrz.de>
2016-02-17 11:37:48 +0100

    added custom configparser

--- src/python/qatool/tests/test_config.py
+++ src/python/qatool/tests/test_config.py
@@ -6,6 +6,7 @@ def test_load_config():
     config.load_configuration()
     config.write_options()
     assert config.get_config_value('settings', 'project_data') == '/tmp/data'
+    assert config.get_config_value('settings', 'qa_results') == '/tmp/output/one/two'
     assert config.get_config_value('settings', 'use_strict') == False
     assert config.get_config_value('settings', 'use_strict2') == None
     assert config.get_config_value('checks', 'cf') == '1.4'
