f9f53ae0cbd0de505ac70b01593f50b22f21695e
Carsten Ehbrecht <ehbrecht@dkrz.de>
2016-02-17 17:11:02 +0100

    added cf module

--- src/python/README.rst
+++ src/python/README.rst
@@ -4,3 +4,5 @@ Quality Assurance Tool for Climate Data
 
 This is the Python wrapper.
 
+It is inspired by https://github.com/ioos/compliance-checker
+
---
+++ src/python/qatool/base.py
@@ -0,0 +1,7 @@
+class BaseCheck(object):
+    def setup(self, ds):
+        """
+        Common setup method for a Checker.
+        Automatically run when running a CheckSuite. Define this method in your Checker class.
+        """
+        pass
