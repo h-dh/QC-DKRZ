1bd1a23087c499723bcec6e35064fd1429ec5553
Carsten Ehbrecht <ehbrecht@dkrz.de>
2016-02-17 17:18:28 +0100

    cleaned up cf and base

--- src/python/qatool/base.py
+++ src/python/qatool/base.py
@@ -1,7 +1,22 @@
 class BaseCheck(object):
-    def setup(self, ds):
+    def __init__(self, verbose=False, criteria='normal', output_filename='-', output_format='text'):
+        self.verbose = verbose
+        self.criteria = criteria
+        self.output_filename = output_filename
+        self.output_format = output_format
+
+    def run(self, ds):
         """
-        Common setup method for a Checker.
-        Automatically run when running a CheckSuite. Define this method in your Checker class.
+        Run all checks on dataset.
+
+        Overwrite this method in your checker.
         """
         pass
+
+
+    def write_output(self, output=None):
+        if self.output_filename == '-':
+            print output
+        else:
+            with open(self.output_filename, 'w') as fp:
+                fp.write(output)
