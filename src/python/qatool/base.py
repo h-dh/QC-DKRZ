8c38895fe1e34b9709981f7e66357f0ff49a1883
Carsten Ehbrecht <ehbrecht@dkrz.de>
2016-02-17 17:24:38 +0100

    setup_runner added

--- src/python/qatool/base.py
+++ src/python/qatool/base.py
@@ -1,9 +1,18 @@
+import os
+
 class BaseCheck(object):
     def __init__(self, verbose=False, criteria='normal', output_filename='-', output_format='text'):
         self.verbose = verbose
         self.criteria = criteria
         self.output_filename = output_filename
         self.output_format = output_format
+        self.setup_runner()
+
+    def setup_runner(self):
+        try:
+            os.remove(self.output_filename)
+        except OSError:
+            pass
 
     def run(self, ds):
         """
@@ -18,5 +27,5 @@ class BaseCheck(object):
         if self.output_filename == '-':
             print output
         else:
-            with open(self.output_filename, 'w') as fp:
+            with open(self.output_filename, 'a') as fp:
                 fp.write(output)
